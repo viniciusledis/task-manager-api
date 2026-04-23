a1e73a2bc51a3d0f69679be692eb5533c3dd08af
Vinicius <vini_ledis@hotmail.com>
2026-04-23 20:39:51 -0300

    feat: adiciona remoção de tarefas

--- src/app.js
+++ src/app.js
@@ -3,3 +3,8 @@ app.post('/tasks', (req, res) => {
   tasks.push(task);
   res.status(201).json(task);
 });
+
+app.delete('/tasks/:id', (req, res) => {
+  tasks.splice(req.params.id, 1);
+  res.sendStatus(204);
+});
