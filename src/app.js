e99c23369ab4f99a0b29018bf127b4baf37bbb28
Vinicius <vini_ledis@hotmail.com>
2026-04-23 20:33:40 -0300

    feat: adiciona atualização de tarefas

--- src/app.js
+++ src/app.js
@@ -3,3 +3,8 @@ app.post('/tasks', (req, res) => {
   tasks.push(task);
   res.status(201).json(task);
 });
+
+app.put('/tasks/:id', (req, res) => {
+  tasks[req.params.id] = req.body;
+  res.json(tasks[req.params.id]);
+});
